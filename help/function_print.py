84103d7b5777045103f0fd026f9708799eb9683b
Alex <alexlisecki2@yandex.ru>
2021-07-01 21:16:03 +0300

    add_function_print

--- help/function_print.py
+++ help/function_print.py
@@ -1,1 +1,38 @@
+# Функция print Python выводит заданные объекты на стандартное устройство вывода (экран) или отправляет
+# их текстовым потоком в файл. Полный синтаксис функции print(): \
+# print(*objects, sep=' ', end='n', file=sys. stdout, flush=False)
+
+
 print('hello')
+# hello
+
+print('hello', 'world', sep='-')
+# hello-world
+
+# sep — это может быть строка, которую необходимо вставлять между значениями, по умолчанию — пробел.
+
+print('hello', 'world', end='b')
+# hello worldb
+
+print(' ')
+# end — это строка, которая добавляется после последнего значения.
+# По умолчанию — это перенос на новую строку (\n).
+# С помощью аргумента end программист может самостоятельно определить окончание выражения print.
+
+item_list = ['world', 'python', 'best']
+
+# вывод списка в одну строку
+for item in item_list:
+    print(item, end=' ')
+# world python best
+
+
+#file — файлоподобный объект (поток). По умолчанию — это sys.stdout.
+# Здесь можно указать файл, в который нужно записать или добавить данные из функции print.
+
+file = open('print.txt','a+')
+
+for item in item_list:
+    print(item,file=file)
+file.close()
+
