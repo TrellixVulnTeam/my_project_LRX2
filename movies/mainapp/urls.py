d51a8b9b7ff8cc481dfa229a16b706a63583bf68
Alex <alexlisecky@gmail.com>
2021-12-08 03:36:15 +0300

    add_some_movies

--- movies/mainapp/urls.py
+++ movies/mainapp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import MoviesView
-
+from .views import MoviesListView, MovieDetailView, AddReview
 
 urlpatterns = [
-    path('', MoviesView.as_view()),
+    path('', MoviesListView.as_view(), name='index'),
+    path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
+    path('review/<int:pk>/', AddReview.as_view(), name='add_review')
 ]
--- movies/mainapp/views.py
+++ movies/mainapp/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .models import Movie
+from .forms import ReviewForm
 
 
-class MoviesView(View):
+class MoviesListView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'mainapp/index.html'
+    context_object_name = 'movies'
+    extra_context = {'title': 'Фильмы'}
 
-    def get(self, request):
-        queryset = Movie.objects.all()
-        return render(request, 'mainapp/index.html', {'movies': queryset})
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = 'url'
+    template_name = 'mainapp/single.html'
+    context_object_name = 'movie'
+
+
+class AddReview(View):
+    """Отзывы"""
+
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
