05d670e54daac5b07350f4947a46e31318d6c2e5
Alex <alexlisecky@gmail.com>
2022-01-11 21:47:28 +0300

    del blog

--- django_site/basic/blog/blog/mainapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import MainListView, ArticleDetailView,ArticleCreateView
-
-urlpatterns = [
-    path('', MainListView.as_view(), name='main'),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
-    path('article/add-article', ArticleCreateView.as_view(), name='add_article')
-]
